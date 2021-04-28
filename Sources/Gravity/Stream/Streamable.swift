cda1d80eee9a04d8e8bed5e8bc16b5fdf9388b9d
Arthur Guiot <arguiot@gmail.com>
2021-04-28 10:15:14 -0400

    Opening class

--- Sources/Gravity/Stream/Streamable.swift
+++ Sources/Gravity/Stream/Streamable.swift
@@ -7,12 +7,24 @@
 
 import Foundation
 
-public class Streamable<Value> {
+open class Streamable<Value> {
     public func encode(value: Value) throws -> Data {
         throw GravityError.EncodeError
     }
     
     public func decode(message: Data) throws -> Value {
         throw GravityError.DecodeError
+    }
+}
+
+public class JSONStreamProcessor<Object>: Streamable<Object> where Object: Codable {
+    public override func encode(value: Object) throws -> Data {
+        let data = try JSONEncoder().encode(value)
+        return data
+    }
+    
+    public override func decode(message: Data) throws -> Object {
+        let json = try JSONDecoder().decode(Object.self, from: message)
+        return json
     }
 }
