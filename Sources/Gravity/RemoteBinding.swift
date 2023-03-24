c2e4b0240cb9151b1d7554f80f73f0d11a47d032
Arthur Guiot <arguiot@gmail.com>
2023-03-24 18:02:41 -0400

    Trying to figure out model that would make binding more safe

--- Sources/Gravity/RemoteBinding.swift
+++ Sources/Gravity/RemoteBinding.swift
@@ -12,13 +12,12 @@ import SwiftUI
 public struct RemoteBinding<Delegate> where Delegate: RemoteObjectDelegate {
     var id: Delegate.Element.ID?
     
-    public subscript<T>(dynamicMember keyPath: WritableKeyPath<Delegate.Element, T>) -> Binding<T?> {
-        return Binding<T?> {
-            guard let id = id else { return nil }
-            return Delegate.shared.store.object(id: id)?[keyPath: keyPath]
+    public subscript<T>(dynamicMember keyPath: WritableKeyPath<Delegate.Element, T>) -> Binding<T>? {
+        guard let id = id else { return nil }
+        guard Delegate.shared.store.object(id: id) != nil else { return nil }
+        return Binding<T> {
+            return Delegate.shared.store.object(id: id)![keyPath: keyPath]
         } set: { newValue, transaction in
-            guard let newValue = newValue else { return }
-            guard let id = id else { return }
             try? Delegate.shared.store.update(id: id) { (object: inout Delegate.Element) in
                 object[keyPath: keyPath] = newValue
             }
@@ -27,23 +26,7 @@ public struct RemoteBinding<Delegate> where Delegate: RemoteObjectDelegate {
 }
 
 public extension Binding {
-    func unwrap<T>(defaultValue: T) -> Binding<T> where Value: OptionalProtocol, Value.Wrapped == T {
-        if let wrappedValue = self.wrappedValue as? T {
-            return Binding<T>(get: { wrappedValue }, set: { newValue in
-                guard let newValue = newValue as? Value else { return }
-                self.wrappedValue = newValue
-            })
-        } else {
-            return (self.wrappedValue as? Binding)?.unwrap(defaultValue: defaultValue) ?? .constant(defaultValue)
-        }
+    func unwrap<T>(defaultValue: T) -> Binding<T> where Value == Optional<T>  {
+        Binding<T>(get: { self.wrappedValue ?? defaultValue }, set: { self.wrappedValue = $0 })
     }
-}
-
-public protocol OptionalProtocol {
-    associatedtype Wrapped
-    var wrappedValue: Wrapped? { get }
-}
-
-extension Optional: OptionalProtocol {
-    public var wrappedValue: Wrapped? { return self }
 }
