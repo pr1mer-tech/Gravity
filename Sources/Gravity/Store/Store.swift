dbd443d76816edd5813f774dff19ef6ad1931e89
Arthur Guiot <arguiot@gmail.com>
2023-03-25 12:19:29 -0400

    Making the request push method available

--- Sources/Gravity/Store/Store.swift
+++ Sources/Gravity/Store/Store.swift
@@ -68,8 +68,8 @@ public class Store<Delegate>: ObservableObject where Delegate: RemoteObjectDeleg
     /// - Parameters:
     ///   - element: Whatever element
     ///   - request: The request you're subscribing to
-    public func add(_ element: Delegate.Element, with request: RemoteRequest<Delegate.Element.ID>) throws {
-        try save(element, with: request, requestPush: false)
+    public func add(_ element: Delegate.Element, with request: RemoteRequest<Delegate.Element.ID>, requestPush: Bool = false) throws {
+        try save(element, with: request, requestPush: requestPush)
     }
     
     /// Updates element in the store, and updates all the views subscribed to the request, or displaying this element.
@@ -77,10 +77,10 @@ public class Store<Delegate>: ObservableObject where Delegate: RemoteObjectDeleg
     ///   - id: The id/key of the element you need to update.
     ///   - request: The request you're subscribing to
     ///   - update: The inout function to update the element.
-    public func update(elementWithID id: Delegate.Element.ID, with request: RemoteRequest<Delegate.Element.ID>, _ update: (inout Delegate.Element) -> Void) throws {
+    public func update(elementWithID id: Delegate.Element.ID, with request: RemoteRequest<Delegate.Element.ID>, requestPush: Bool = false, _ update: (inout Delegate.Element) -> Void) throws {
         guard var object = self.object(id: id) else { return }
         update(&object)
-        try self.save(object, with: request, requestPush: false)
+        try self.save(object, with: request, requestPush: requestPush)
     }
     
     func update(id: T.ID, with request: RemoteRequest<T.ID>, _ update: (inout T) -> Void) throws {
